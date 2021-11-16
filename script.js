3ddd53b3faa9fc3435924bfeac0187bbe809369b
Cher Lafferty <cherlafferty01@gmail.com>
2021-11-15 17:06:22 -0800

    origin

---
+++ script.js
@@ -0,0 +1,9 @@
+function logout(element) {
+    element.innerText = "Lougout";
+}
+function hide(element) {
+    element.remove();
+}
+/*function alert(element) {
+    element.alert="this page says Ninja was liked"
+}*/
